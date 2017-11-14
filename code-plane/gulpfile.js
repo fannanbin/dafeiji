db7c98a4c0fc51aa9bfdf54157a12d7ad6580ff8
nan <user@1000phone.com>
2017-11-14 11:00:19 +0800

    添加了code-plane文件

---
+++ code-plane/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp");
+var htmlmin = require("gulp-htmlmin");
+var  obj = {
+	removeComments: true, //清除HTML注释
+	collapseWhitespace: true, //压缩HTML
+	collapseBooleanAttributes: true,//省略布尔属性的值<input checked="true"/> ==> <input checked/>
+	removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+	removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
+	removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+	minifyJS: true, //压缩页面JS
+	minifyCSS: true //压缩页面CSS
+}
+
+gulp.task("jstask",function(){
+	gulp.src("03_rand.html")
+    .pipe( htmlmin(obj))
+    .pipe( gulp.dest("css") )
+})
+
+
+gulp.task('default',["jstask"]);
